372112689dd39f34a452b5895c7a5659e4330eec
amarcin <102642895+amarcin@users.noreply.github.com>
2024-10-05 12:41:07 -0500

    test button

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,7 +85,7 @@ with dataTab:
     st.header("Current Data")
     display_data()
     # Add a refresh button at the bottom of the chart  
-    if st.button("Refresh Data", icon="🔄"):  
+    if st.button("", icon="🔄"):  
         # Clear the cache and fetch the data again  
         fetch_units.clear()  
         display_data()  
